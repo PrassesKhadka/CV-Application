9449900886db24c867b31b9dd91f0a859726e1f4
Prasses Khadka <khadkaprasses@gmail.com>
2023-05-11 09:04:31 +0545

    Error corrected due to infinite re rendering caused due to explicit function call

--- cv-project/src/components/Personal.js
+++ cv-project/src/components/Personal.js
@@ -4,49 +4,77 @@ import formAppear from './Form';
 class Personal extends Component{
     constructor()
     {
-        super()
+        super();
         this.state={
-            edit:false,
             personal:{
-                name:'Full Name',
-                email:'example@gmail.com',
-                phone:'9863490421',
+                name:['Full Name',false],
+                email:['example@gmail.com',false],
+                phone:['9863490421',false],
             }
-        }
+        };
+        this.edit=this.edit.bind(this)
+        this.onSubmit=this.onSubmit.bind(this)
+        this.handleChange=this.handleChange.bind(this)
+
     }
 
-    edit=()=>{
-        this.setState({
-            edit:true,
-        })
-        
-    }
-    onSubmit=(e)=>{
-        e.preventDefault()
-        this.setState({
-            edit:false,
-        })
-    }
-    handleChange=(e)=>{
-        this.setState({
+    edit=(key)=>{
+        this.setState(prevState=>({
             personal:{
-                name:e.target.value,
+                ...prevState.personal,
+                [key]:[prevState.personal[key][0],true],
             }
-        })
+        }))
+    }
+    onSubmit=(e,key)=>{
+        e.preventDefault()
+        this.setState(prevState=>({
+            personal:{
+                ...prevState.personal,
+                [key]:[prevState.personal[key][0],false],
+            }
+        }))
+    }
+    handleChange=(e,key)=>{
+        this.setState(prevState=>({
+            personal:{
+                ...prevState.personal,
+                [key]:[e.target.value,prevState.personal[key][1]],
+            }
+        }))
     }
 
     render()
     {
         //Destructuring state
-        const {edit,personal}=this.state;
+        const {personal}=this.state;
         return(
             <div>
-                <span onClick={this.edit}>{edit ?
-                <form onSubmit={this.onSubmit}>
-                    <input onChange={this.handleChange} type='text'></input>
+                {/* For Name */}
+                <span onClick={()=>this.edit('name')}>{personal.name[1] ?
+                <form onSubmit={(e)=>this.onSubmit(e,'name')}>
+                    <input onChange={(e)=>this.handleChange(e,'name')} type='text'></input>
                     <button type="submit">Ok</button>
                 </form>
-                :personal.name}
+                :personal.name[0]}
+                </span>
+                <br></br>
+                {/* For email */}
+                <span onClick={()=>this.edit('email')}>{personal.email[1] ?
+                <form onSubmit={(e)=>this.onSubmit(e,'email')}>
+                    <input onChange={(e)=>this.handleChange(e,'email')} type='text'></input>
+                    <button type="submit">Ok</button>
+                </form>
+                :personal.email[0]}
+                </span>
+                <br></br>
+                {/* For phone */}
+                <span onClick={()=>this.edit('phone')}>{personal.phone[1] ?
+                <form onSubmit={(e)=>this.onSubmit(e,'phone')}>
+                    <input onChange={(e)=>this.handleChange(e,'phone')} type='text'></input>
+                    <button type="submit">Ok</button>
+                </form>
+                :personal.phone[0]}
                 </span>
             </div>
         )
--- cv-project/src/components/Skills.js
+++ cv-project/src/components/Skills.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Skill=()=>{
+const Skills=()=>{
 
 }
-export default Skill;
+export default Skills;
